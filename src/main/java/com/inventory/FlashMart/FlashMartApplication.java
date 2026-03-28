fe4dc6938e88ef8d86b360ae6eeba46273c5be71
Mohammad Anas <mohammadanas8948@gmail.com>
2026-03-28 19:56:29 +0530

    first commit

---
+++ src/main/java/com/inventory/FlashMart/FlashMartApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.FlashMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashMartApplication.class, args);
+	}
+
+}
