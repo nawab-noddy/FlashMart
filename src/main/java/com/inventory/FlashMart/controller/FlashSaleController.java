d177846de691e016eb2863a12fe889a8835ad9ee
Mohammad Anas <mohammadanas8948@gmail.com>
2026-04-07 22:02:40 +0530

    Controller is Completed

--- src/main/java/com/inventory/FlashMart/controller/FlashSaleController.java
+++ src/main/java/com/inventory/FlashMart/controller/FlashSaleController.java
@@ -2,6 +2,7 @@ package com.inventory.FlashMart.controller;
 
 import com.inventory.FlashMart.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,16 @@ public class FlashSaleController {
     private InventoryService inventoryService;
 
     @PostMapping("/buy")
-    public ResponseEntity<?>
+    public ResponseEntity<?> accessInventory(){
+        boolean serviceResponse = inventoryService.deductStock();
+
+        if(serviceResponse){
+            return ResponseEntity.ok("Success");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Sorry U are late");
+        }
+    }
 
 
 }
